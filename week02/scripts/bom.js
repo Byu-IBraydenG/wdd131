1af0ab460ae165cc94de4ad31579dd4987a828fc
Byu-IBraydenG <dragon.grant03@gmail.com>
2024-09-23 11:25:09 -0600

    additional code for JS

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -11,3 +11,29 @@ deleteButton.textContent = '❌';
 
 li.append(deleteButton);
 list.append(li);
+
+button.addEventListener('click', function () {
+    if (input.value.trim() !== '') {
+        const input = document.querySelector('#favchap');
+        const button = document.querySelector('button');
+        const list = document.querySelector('_____');
+
+        const li = document.createElement('li');
+
+        const deleteButton = document.createElement('button');
+        li.textContent = input.value;
+
+        deleteButton.textContent = '❌';
+
+        li.append(deleteButton);
+        list.append(li);
+    }
+
+    deleteButton.addEventListener('click', function () {
+        list.removeChild(li)
+        input.focus();
+    });
+
+    input.value = '';
+    input.focus();
+})
